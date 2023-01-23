8b3c70507e7b8cc074ef976df93a0a441d722a2a
dtahiri <dusan.tahiri@clarivate.com>
2023-01-23 16:03:59 +0100

    render function changed

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -7,27 +7,18 @@ import (
 	"net/http"
 )
 
-// RenderTemplate renders a template
-func RenderTemplateTEst(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template:", err)
-	}
-}
+var templateCache = make(map[string]*template.Template)
 
-var tc = make(map[string]*template.Template)
-
-func RenderTemplate(w http.ResponseWriter, t string) {
+func RenderTemplate(w http.ResponseWriter, temp string) {
 	var tmpl *template.Template
 	var err error
 
 	// check to see if we already have the template in our cache
-	_, inMap := tc[t]
+	_, inMap := templateCache[temp]
 	if !inMap {
 		// need to create the template
 		log.Println("creating template and adding to cache")
-		err = createTemplateCache(t)
+		err = createTemplateCache(temp)
 		if err != nil {
 			log.Println(err)
 		}
@@ -36,7 +27,7 @@ func RenderTemplate(w http.ResponseWriter, t string) {
 		log.Println("using cached template")
 	}
 
-	tmpl = tc[t]
+	tmpl = templateCache[temp]
 
 	err = tmpl.Execute(w, nil)
 	if err != nil {
@@ -57,7 +48,7 @@ func createTemplateCache(t string) error {
 	}
 
 	// add template to cache (map)
-	tc[t] = tmpl
+	templateCache[t] = tmpl
 
 	return nil
 }
