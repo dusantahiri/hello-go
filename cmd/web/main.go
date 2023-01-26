7dee89dbd58914abf788bab4fc134c9b0f2359f9
dtahiri <dusan.tahiri@clarivate.com>
2023-01-26 17:09:24 +0100

    session manager added

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/dusantahiri/hello-go/pkg/config"
 	"github.com/dusantahiri/hello-go/pkg/handlers"
 	"github.com/dusantahiri/hello-go/pkg/render"
@@ -12,10 +14,21 @@ import (
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 // main is the main function
 func main() {
 
-	var app config.AppConfig
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -29,18 +29,24 @@ func NewHandlers(r *Repository) {
 }
 
 // Home is the handler for the home page
-func (m *Repository) Home(response http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(response, "home.page.tmpl", &models.TemplateData{})
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 // About is the handler for the about page
-func (m *Repository) About(response http.ResponseWriter, r *http.Request) {
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	// perform some logic
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello, again"
 
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
 	// send data to the template
-	render.RenderTemplate(response, "about.page.tmpl", &models.TemplateData{
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
 	})
 }
